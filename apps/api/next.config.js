2a60e8d92cbc059445c4a5bcc726df66fe63c39b
huweijian <598307691@qq.com>
2024-06-03 17:11:03 +0800

    修复prisma在monorepo下运行的429问题

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
 module.exports = {
   transpilePackages: ['@repo/ui'],
   output: 'standalone',
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
+  // https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-monorepo
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
   },
 };
